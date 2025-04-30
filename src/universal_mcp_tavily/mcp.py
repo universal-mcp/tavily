f07214bec88b02f6d9bfd07fe9f85b56d631f865
Manoj Bajaj <manojbajaj95@gmail.com>
2025-04-30 12:00:40 +0530

    feat: Initial commit from MCP CLI init command

---
+++ src/universal_mcp_tavily/mcp.py
@@ -0,0 +1,19 @@
+
+from universal_mcp.servers.server import SingleMCPServer
+from universal_mcp.integrations.integration import ApiKeyIntegration
+from universal_mcp.stores.store import EnvironmentStore
+
+from universal_mcp_tavily.app import TavilyApp
+
+env_store = EnvironmentStore()
+integration_instance = ApiKeyIntegration(name="TAVILY_API_KEY", store=env_store)
+app_instance = TavilyApp(integration=integration_instance)
+
+mcp = SingleMCPServer(
+    app_instance=app_instance,
+)
+
+if __name__ == "__main__":
+    mcp.run()
+
+
